17d7658b04b95a46c3841abf6fb5ea93c6f2d79b
Philip Newborough <corenominal@corenominal.org>
2022-07-29 15:14:53 +0100

    Add a basic Sass structure

---
+++ assets/sass/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Using Sass
+# You can install Sass on Windows, Mac, or Linux by downloading the package for
+# your operating system from GitHub and adding it to your PATH. That’s all -- 
+# there are no external dependencies and nothing else you need to install. 
+# See: https://github.com/sass/dart-sass/releases
+
+sass -w main.scss ../css/main.css
+
+exit
